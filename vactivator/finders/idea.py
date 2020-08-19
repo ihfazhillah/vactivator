3498f1c4c3035a068067ea66724a0b95d87347b2
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-19 13:35:22 +0700

    remove lxml, use ElementTree instead

--- vactivator/finders/idea.py
+++ vactivator/finders/idea.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-
-from lxml import etree
+from xml.etree.ElementTree import ElementTree
 
 
 class IdeaFinder:
@@ -14,10 +13,12 @@ class IdeaFinder:
     def get_venv_activator(self):
         workspace = self.path / ".idea" / "workspace.xml"
         with workspace.open("r") as workspace_xml:
-            root = etree.parse(workspace_xml)
+            # root = etree.parse(workspace_xml)
 
             # only work on unix like. how about windows ???
             home = Path.home()
+            etree = ElementTree()
+            root = etree.parse(source=workspace_xml)
             python_path = root.find(".//option[@name='mySdkHome']").attrib["value"]
 
             abs_python_path = python_path.replace("$USER_HOME$", str(home))
